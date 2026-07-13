e5beb496986a7849272c11dcd98f2d14451cf96c
Sohrab Alam Ansari <ansari.shohrab80@gmail.com>
2026-07-13 11:54:26 +0530

    Refactor server.js for consistent quotation marks and update CORS configuration

--- youtube-backend/server.js
+++ youtube-backend/server.js
@@ -1,22 +1,25 @@
-require('dotenv').config();
+require("dotenv").config();
 const express = require("express");
-const cors = require('cors');
+const cors = require("cors");
 const runMigrations = require("./migrations/migrate");
-const videoRoutes = require('./routes/video.routes');
-const commentRoutes = require('./routes/comment.routes');
-const authRoutes = require('./routes/auth.routes');
+const videoRoutes = require("./routes/video.routes");
+const commentRoutes = require("./routes/comment.routes");
+const authRoutes = require("./routes/auth.routes");
 const channelRoutes = require("./routes/channel.routes");
 
 
 const app = express();
 app.use(express.json());
 app.use(
-    cors({
-        origin: 'http://localhost:5173'
-    })
+  cors({
+    origin: [
+      "http://localhost:5173",
+      "https://youtube-9tzpi36vp-sohrabalam.vercel.app",
+    ],
+  }),
 );
-app.use('/auth', authRoutes);
-app.use('/videos', videoRoutes);
+app.use("/auth", authRoutes);
+app.use("/videos", videoRoutes);
 app.use("/videos", commentRoutes);
 app.use("/channels", channelRoutes);
 
