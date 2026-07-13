5954c972001142acf5d0f94fbdb8423e377ee562
Sohrab Alam Ansari <ansari.shohrab80@gmail.com>
2026-07-13 12:06:57 +0530

    Update CORS configuration to allow all Vercel subdomains

--- youtube-backend/server.js
+++ youtube-backend/server.js
@@ -12,10 +12,7 @@ const app = express();
 app.use(express.json());
 app.use(
   cors({
-    origin: [
-      "http://localhost:5173",
-      "https://youtube-chi-drab.vercel.app/",
-    ],
+    origin: ["http://localhost:5173", /^https?:\/\/(?:.+\.)?vercel\.app$/],
   }),
 );
 app.use("/auth", authRoutes);
